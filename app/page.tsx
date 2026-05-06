ce7200fe7e4cc79c1c3392c51ba9ce5feef523f8
piowarior <muhamadrohisuliman@gmail.com>
2026-05-06 21:33:32 +0700

    update tampilan

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,10 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { trackDevice } from "@/lib/tracker";
+import Header from "@/components/Header";
 
 export default function Home() {
-  const [logs, setLogs] = useState<any[]>([]);
-
-  useEffect(() => {
-    const secret = "aec0fcb4-7cb6-4446-a05a-44eaf569b16d";
-    trackDevice(secret);
-
-    // ambil data tiap 3 detik
-    const interval = setInterval(async () => {
-      const res = await fetch("/api/device/ping");
-      const data = await res.json();
-      setLogs(data);
-    }, 3000);
-
-    return () => clearInterval(interval);
-  }, []);
 
   return (
-    <div>
-      <h1>Tracking Device Aktif 🚀</h1>
-
-      <h2>Data Lokasi:</h2>
-
-      {logs.map((log, i) => (
-        <div key={i} style={{ border: "1px solid #ccc", margin: 10 }}>
-          <p>IP: {log.ip}</p>
-          <p>Kota: {log.city}</p>
-          <p>Latitude: {log.latitude}</p>
-          <p>Longitude: {log.longitude}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <Header />
+    </>
   );
-}
+} 
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
 
+import React, { useEffect, useState } from "react";
 const Header = () => {
+  
   return (
-    <div>
-      
+   <>
+   <nav className="w-[100vw] h-[100vh] bg-[#1B1B1B] " style={{
+  background: "radial-gradient(circle, #133559 0%, #0C2644 100%)"}}> 
+    <div className="">
+      <ul>
+        <li><a href="#"><img className=" w-[100px] " src="/logo/sentinel_logo.png" alt="logo" /></a></li>
+      </ul>
     </div>
-  )
-}
+   </nav>
+   </>
+  );
+};
 
-export default Header
+export default Header;
