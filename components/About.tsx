b33c4d5e29c35943e53595bca4cb121b285196c3
piowarior <muhamadrohisuliman@gmail.com>
2026-06-04 23:07:40 +0700

    pemanasan otak

--- components/About.tsx
+++ components/About.tsx
@@ -13,13 +13,16 @@ const About = () => {
       <div className="container mx-auto px-[50px] flex mt-28">
         <div className="w-[50%] flex flex-col justify-center">
           <h2 className='text-5xl leading-17.5 font-semibold text-white max-lg:text-[45px] max-md:text-3xl max-lg:leading-10'>Sentinel <span className='font-thin'>Track</span></h2>
-          <p>SentinelTrack adalah platform pemantauan perangkat berbasis web yang dirancang untuk membantu pengguna melacak posisi dan status aset digital mereka secara langsung dari satu pintu.</p>
+          <p className="text-white text-base mt-5 font-light">SentinelTrack adalah platform pemantauan perangkat berbasis web yang dirancang untuk membantu pengguna melacak posisi dan status aset digital mereka secara langsung dari satu pintu.</p>
           <div className="">
             <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
+              <li className='flex flex-col transition'><details className="decoration-0"> <summary>1. Pemantauan Mandiri</summary> <h1>haaalo</h1></details> <svg width="550" height="1" viewBox="0 0 550 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <line y1="0.5" x2="550" y2="0.5" stroke="white"/>
+              </svg>
+              </li>
+              <li><details className="decoration-0"> <h1>haaalo</h1></details></li>
+              <li><details className="decoration-0"> <h1>haaalo</h1></details></li>
+              <li><details className="decoration-0"> <h1>haaalo</h1></details></li>
             </ul>
           </div>
         </div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import "@/style/global.css";
+import { gsap } from "gsap";
 
 
 
